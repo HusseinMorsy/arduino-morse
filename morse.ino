e4031d4a4bf62e78a4278761c4ca5991be34a56a
Hussein Morsy <hussein@morsy.de>
2018-09-08 23:36:37 +0200

    Add all characters and add hussein as an example

--- morse.ino
+++ morse.ino
@@ -8,36 +8,93 @@ void setup() {
 }
 
 void loop() {
-  morse("esma");
+  morse("hussein");
   delay(3000);
 }
 void morse(String str)
 {
   for(int i=0;i<str.length();i++)
   {
+
     switch (str[i]){
       case 'a':
       code(".-"); break;
       case 'b':
-      code(".-"); break;
+      code("-..."); break;
       case 'c':
       code("-.-."); break;
       case 'd':
       code("-.."); break;
       case 'e':
       code("."); break;
-      case 'm':
-      code("--"); break;
-      case 'o':
-      code("---"); break;
-      case 's':
-      code("..."); break;
+      case 'f':
+      code("..-."); break;
+      case 'g':
+      code("--."); break;
+      case 'h':
+      code("...."); break;
+      case 'i':
+      code(".."); break;
+      case 'j':
+      code(".---"); break;
       case 'k':
       code("-.-"); break;
+      case 'l':
+      code(".-.."); break;
+      case 'm':
+      code("--"); break;
+      case 'n':
+      code("-."); break;
+      case 'o':
+      code("---"); break;
+      case 'p':
+      code(".--."); break;
+      case 'q':
+      code("--.-"); break;
+      case 'r':
+      code(".-."); break;
+      case 's':
+      code("..."); break;
       case 't':
       code("-"); break;
+      case 'u':
+      code("..-"); break;
+      case 'v':
+      code("...-"); break;
+      case 'w':
+      code(".--"); break;
+      case 'x':
+      code("-..-"); break;
       case 'y':
       code("-.--"); break;
+      case 'z':
+      code("--.."); break;
+      case '1':
+      code(".----"); break;
+      case '2':
+      code("..---"); break;
+      case '3':
+      code("...--"); break;
+      case '4':
+      code("....-"); break;
+      case '5':
+      code("....."); break;
+      case '6':
+      code("-...."); break;
+      case '7':
+      code("--..."); break;
+      case '8':
+      code("---.."); break;
+      case '9':
+      code("----."); break;
+      case '0':
+      code("-----"); break;
+      case '.':
+      code(".-.-.-"); break;
+      case ',':
+      code("--..--"); break;
+      case '?':
+      code("..--.."); break;
       case ' ':
       pause(); break;
     }
